97b905e162958c0525a57e34644a5cddeea73794
StudentArg <99406669+StudentArg@users.noreply.github.com>
2022-04-08 05:23:22 +0100

    Add files via upload

---
+++ GetIP/GetIP/Grabber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetIP
+{
+    internal class Grabber
+    {
+        public static string GetIp()
+        {
+            string ip = new WebClient().DownloadString("https://api.ipify.org/");
+            return ip;
+        }
+    }
+}
